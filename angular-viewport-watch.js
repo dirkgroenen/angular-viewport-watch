04cd4de1a7d92bc6d2fe2d8d9ea49739f3209b63
Jeff Forde and Shy Aberman <pair+jeffrey+shy@metabrite.com>
2016-09-13 15:17:12 -0700

    Fix indenting of `link()` code

--- angular-viewport-watch.js
+++ angular-viewport-watch.js
@@ -11,86 +11,86 @@
         }
 
         var link = function(scope, element, attr) {
-                if($parse(attr.viewportWatch)(scope) == false){
-                    return false;
-                }
+            if($parse(attr.viewportWatch)(scope) == false){
+                return false;
+            }
 
-                if (attr.hashkey !== element.attr('hashkey')) {
-                    var unwatch = scope.$watch(function() {
-                        return element.attr('hashkey');
-                    }, function() {
-                        unwatch && unwatch();
-                        link.call(this, scope, element, attr);
-                    });
-                    return true;
-                }
+            if (attr.hashkey !== element.attr('hashkey')) {
+                var unwatch = scope.$watch(function() {
+                    return element.attr('hashkey');
+                }, function() {
+                    unwatch && unwatch();
+                    link.call(this, scope, element, attr);
+                });
+                return true;
+            }
 
-                var container = (attr.viewportWatchContainer && attr.viewportWatchContainer.length > 1) ? attr.viewportWatchContainer : undefined;
+            var container = (attr.viewportWatchContainer && attr.viewportWatchContainer.length > 1) ? attr.viewportWatchContainer : undefined;
 
-                var elementWatcher = scrollMonitor.create(element, scope.$eval(attr.viewportWatch || "0"), container);
+            var elementWatcher = scrollMonitor.create(element, scope.$eval(attr.viewportWatch || "0"), container);
 
-                function watchDuringDisable() {
-                    this.$$watchersBackup = this.$$watchersBackup || [];
-                    this.$$watchers = this.$$watchersBackup;
-                    var unwatch = this.constructor.prototype.$watch.apply(this, arguments);
-                    this.$$watchers = null;
-                    return unwatch;
-                }
-                function toggleWatchers(scope, enable) {
-                    var digest, current, next = scope;
-                    do {
-                        current = next;
-                        if (enable) {
-                            if (current.hasOwnProperty("$$watchersBackup")) {
-                                current.$$watchers = current.$$watchersBackup;
-                                delete current.$$watchersBackup;
-                                delete current.$watch;
-                                digest = !scope.$root.$$phase;
-                            }
+            function watchDuringDisable() {
+                this.$$watchersBackup = this.$$watchersBackup || [];
+                this.$$watchers = this.$$watchersBackup;
+                var unwatch = this.constructor.prototype.$watch.apply(this, arguments);
+                this.$$watchers = null;
+                return unwatch;
+            }
+            function toggleWatchers(scope, enable) {
+                var digest, current, next = scope;
+                do {
+                    current = next;
+                    if (enable) {
+                        if (current.hasOwnProperty("$$watchersBackup")) {
+                            current.$$watchers = current.$$watchersBackup;
+                            delete current.$$watchersBackup;
+                            delete current.$watch;
+                            digest = !scope.$root.$$phase;
+                        }
+                    } else {
+                        if (!current.hasOwnProperty("$$watchersBackup")) {
+                            current.$$watchersBackup = current.$$watchers;
+                            current.$$watchers = null;
+                            current.$watch = watchDuringDisable;
+                        }
+                    }
+                    next = current.$$childHead;
+                    while (!next && current !== scope) {
+                        if (current.$$nextSibling) {
+                            next = current.$$nextSibling;
                         } else {
-                            if (!current.hasOwnProperty("$$watchersBackup")) {
-                                current.$$watchersBackup = current.$$watchers;
-                                current.$$watchers = null;
-                                current.$watch = watchDuringDisable;
-                            }
+                            current = current.$parent;
                         }
-                        next = current.$$childHead;
-                        while (!next && current !== scope) {
-                            if (current.$$nextSibling) {
-                                next = current.$$nextSibling;
-                            } else {
-                                current = current.$parent;
-                            }
-                        }
-                    } while (next);
-                    if (digest) {
-                        scope.$digest();
                     }
+                } while (next);
+                if (digest) {
+                    scope.$digest();
                 }
-                function disableDigest() {
-                    toggleWatchers(scope, false);
-                }
-                function enableDigest() {
-                    toggleWatchers(scope, true);
-                }
-                function disableDigestingIfOutsideViewport() {
-                    if (!elementWatcher.isInViewport) {
-                        disableDigest();
-                        debouncedViewportUpdate();
-                    }
-                }
-                scope.$applyAsync(disableDigestingIfOutsideViewport);
-
-                elementWatcher.enterViewport(enableDigest);
-                elementWatcher.exitViewport(disableDigest);
-                scope.$on("toggleWatchers", function(event, enable) {
-                    toggleWatchers(scope, enable);
-                });
-                scope.$on("$destroy", function() {
-                    elementWatcher.destroy();
+            }
+            function disableDigest() {
+                toggleWatchers(scope, false);
+            }
+            function enableDigest() {
+                toggleWatchers(scope, true);
+            }
+            function disableDigestingIfOutsideViewport() {
+                if (!elementWatcher.isInViewport) {
+                    disableDigest();
                     debouncedViewportUpdate();
-                });
-            };
+                }
+            }
+            scope.$applyAsync(disableDigestingIfOutsideViewport);
+
+            elementWatcher.enterViewport(enableDigest);
+            elementWatcher.exitViewport(disableDigest);
+            scope.$on("toggleWatchers", function(event, enable) {
+                toggleWatchers(scope, enable);
+            });
+            scope.$on("$destroy", function() {
+                elementWatcher.destroy();
+                debouncedViewportUpdate();
+            });
+        };
 
         return {
             restrict: "AE",
